0212a236dd8af5a1309e0e0e90979317b92c1c43
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-05-18 15:22:03 -0300

    duplicate images

--- 02-modulo-javascript/memory-game/app.js
+++ 02-modulo-javascript/memory-game/app.js
@@ -17,6 +17,16 @@ const getData = () => [
   { imgSrc: './images/tuco-salamanca.webp', name: 'Tuco Salamanca' },
   { imgSrc: './images/walter-white-jr.webp', name: 'Walter White Jr' },
   { imgSrc: './images/walter-white.webp', name: 'Walter White' },
+  { imgSrc: './images/gustavo-fring.jpeg', name: 'Gustavo Fring' },
+  { imgSrc: './images/hank-schrader.webp', name: 'Hank Schrader' },
+  { imgSrc: './images/jane-margolis.webp', name: 'Jane Margolis' },
+  { imgSrc: './images/jesse-pinkman.webp', name: 'Jesse Pinkman' },
+  { imgSrc: './images/mike-ehrmantraut.webp', name: 'Mike Ehrmantraut' },
+  { imgSrc: './images/saul-goodman.webp', name: 'Saul Goodman' },
+  { imgSrc: './images/skylar-white.webp', name: 'Skylar White' },
+  { imgSrc: './images/tuco-salamanca.webp', name: 'Tuco Salamanca' },
+  { imgSrc: './images/walter-white-jr.webp', name: 'Walter White Jr' },
+  { imgSrc: './images/walter-white.webp', name: 'Walter White' },
 ];
 
 const randomize = () => {
